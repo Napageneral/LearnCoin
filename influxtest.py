6ea4a2c0736329d89030011e61d90a2ab0acf576
Connor <connormcn37@gmail.com>
2017-07-19 03:19:53 -0700

    GDAX orderbook db
    Not sure if this is a functional commit, but on my system I have it working in a different directory. Don't pip install gdax, although you may need to install the dependencies. The pip install version of gdax seemed to be broken. If you get pull their repo it works.

---
+++ influxtest.py
@@ -0,0 +1,62 @@
+import influxdb
+from order_book import OrderBook
+import time
+import json
+from decimal import Decimal
+
+'''
+class MySeriesHelper(influxdb.SeriesHelper):
+   class Meta:
+      db = mydb
+      series_name = 'events'
+'''    
+      
+      
+def main():
+   mydb = influxdb.InfluxDBClient(database='new')
+   
+   #ob = OrderBook()
+   #old = ob.on_message
+   
+   #def on_message_hook(message):
+   #   mydb.write_points([{"measurement": "tick","fields": message}])
+   #   old(message)
+      
+   #ob.on_message = on_message_hook
+   
+   class InfluxOrderBook(OrderBook):
+      
+      def __init__(self, product_id=None):
+         super(InfluxOrderBook, self).__init__(product_id=product_id)
+         
+         print("init influxorderbook")
+
+      def match(self, order):
+         super(InfluxOrderBook, self).match(order)
+         
+         size = float(order['size'])
+         price = float(order['price'])
+         print("match: " + "size: " + str(size) + " price: " + str(price))
+         mydb.write_points([{"measurement":"match","fields":{"size":size,"price":price}}])
+   
+      def add(self, order):
+         super(InfluxOrderBook, self).add(order)
+         
+         fields = {
+            'side': order['side'],
+            'size': float(order.get('size') or order['remaining_size']),
+            'price': float(order['price'])
+         }
+         
+         print("add: side:"+str(order['side']) + " size: " + str(float(order.get('size') or order['remaining_size'])) + " price: " + str(float(order['price'])))
+         mydb.write_points([{"measurement":"add", "fields":fields}])
+      
+   ob = InfluxOrderBook()
+   
+   ob.start()
+   time.sleep(100)
+   ob.close()
+
+'''
+{'sequence': 3610668509, 'price': '2041.91000000', 'time': '2017-07-17T11:13:25.977000Z', 'remaining_size': '0.50000000', 'side': 'buy', 'product_id': 'BTC-USD', 'type': 'open', 'order_id': 'ba8366c1-fc6c-41a4-9389-ab9672f130c3'}
+'''
