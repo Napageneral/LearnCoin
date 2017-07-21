c9aedf73833102f8bf9299f181a32c27d0841fd1
Connor <connormcn37@gmail.com>
2017-07-21 01:41:46 -0700

    stuff and things

--- influxtest.py
+++ influxtest.py
@@ -1,30 +1,69 @@
 import influxdb
 from order_book import OrderBook
-import time
+import datetime, time
 import json
 from decimal import Decimal
       
 def main():
-   mydb = influxdb.InfluxDBClient(database='new')
+   mydb = influxdb.InfluxDBClient()
    
    class InfluxOrderBook(OrderBook):
       
       def __init__(self, product_id=None):
          super(InfluxOrderBook, self).__init__(product_id=product_id)
-         
+         self.dbname = "gdax."+datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+         mydb.create_database(self.dbname)
          print("init influxorderbook")
-
+         
+         
+      def on_message(self, message):
+         super(InfluxOrderBook, self).on_message(message)         
+            
+      def remove(self, order):
+         super(InfluxOrderBook, self).remove(order)
+         
+         if order['reason'] == 'canceled':
+               mydb.write_points([{"measurement":"message",
+                                   "fields":{
+                                       "price": float(order['price']),
+                                       "size": -float(order.get('size') or order['remaining_size']),
+                                       "side": order['side'],
+                                       "time": order['time']
+                                    },
+                                    }], database=self.dbname)
+         
       def match(self, order):
          super(InfluxOrderBook, self).match(order)
-         
-         size = float(order['size'])
-         price = float(order['price'])
-         print("match: " + "size: " + str(size) + " price: " + str(price))
-         mydb.write_points([{"measurement":"match","fields":{"size":size,"price":price}}])
+         mydb.write_points([{"measurement":"message",
+                                 "fields":{
+                                    "price": float(order['price']),
+                                    "size": -float(order.get('size') or order['remaining_size']),
+                                    "side": order['side'],
+                                    "time": order['time']
+                                 },
+                               }], database=self.dbname)
    
       def add(self, order):
          super(InfluxOrderBook, self).add(order)
          
+         if not 'time' in order:
+            mydb.write_points([{"measurement":"message",
+                                 "fields":{
+                                    "price": float(order['price']),
+                                    "size": float(order.get('size') or order['remaining_size']),
+                                    "side": order['side'],
+                                    },
+                                 }], database=self.dbname)
+         else:
+            mydb.write_points([{"measurement":"message",
+                                    "fields":{
+                                       "price": float(order['price']),
+                                       "size": float(order.get('size') or order['remaining_size']),
+                                       "side": order['side'],
+                                       "time": order['time']
+                                       },
+                                    }], database=self.dbname)
+         '''
          fields = {
             'side': order['side'],
             'size': float(order.get('size') or order['remaining_size']),
@@ -33,12 +72,12 @@ def main():
          
          print("add: side:"+str(order['side']) + " size: " + str(float(order.get('size') or order['remaining_size'])) + " price: " + str(float(order['price'])))
          mydb.write_points([{"measurement":"add", "fields":fields}])
-      
+         '''
    ob = InfluxOrderBook()
    
    ob.start()
-   time.sleep(100)
-   ob.close()
+   #time.sleep(100)
+   #ob.close()
 
 if __name__ == '__main__':
    main()
